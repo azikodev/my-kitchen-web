71b17027647acfe0076857db395f0f7dee6360ea
azizbek <aziko.coder@gmail.com>
2024-07-11 09:25:48 +0500

    about page edited

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 function About() {
-  return (
-    <div>About</div>
-  )
+  let user = useSelector((state) => state.user.user);
+  let lastLoginAt = user.createdAt;
+  const lastLoginDate = new Date(parseInt(lastLoginAt));
+  const formatDate = (timestamp) => {
+    if (!timestamp) return "";
+
+    const date = new Date(parseInt(timestamp));
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+
+    return date.toLocaleString(undefined, options);
+  };
+
+  const formattedDate = formatDate(user.createdAt);
+  const datePart = formattedDate.split(",")[0];
+  const timePart = formattedDate.split(",")[1];
+
+  if (user) {
+    return (
+      <div className="align-content my-10 pb-10 flex gap-5 items-start justify-center md:flex-row flex-col ">
+        <div className="">
+          <img
+            className="size-52 rounded-full md:ml-24 md:mt-12 lg:m-0"
+            src={user.photoURL}
+            alt=""
+          />
+        </div>
+        <div className="pt-10 flex flex-col gap-3 items-center justify-center ">
+          <div className="stats shadow max-w-xl lg:flex-row flex-col flex glass mx-auto">
+            <div className="stat ">
+              <div className="stat-figure text-primary"></div>
+              <div className="stat-title text-[20px]">Name</div>
+              <div className="stat-value text-green-400">{user.displayName}</div>
+              <div className="stat-desc  font-bold text-xs pt-2">
+                <span className="text-2xl">Email: </span><span className="text-xl">{user.email}</span>
+              </div>
+            </div>
+            {" "}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default About
+export default About;
