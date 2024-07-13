912b77e34dd389d26ade151102830ab1feefea4f
azizbek <aziko.coder@gmail.com>
2024-07-13 18:11:42 +0500

    git push

--- src/pages/LookProduct.jsx
+++ src/pages/LookProduct.jsx
@@ -1,13 +1,17 @@
 //react icons
 import { IoIosGitNetwork } from "react-icons/io";
 
+import buildingIMG from "../assets/undraw_building_websites_i78t.svg"
+import { Link } from "react-router-dom";
 function LookProduct() {
     return (
-        <div className='max-container [w-1200px] mx-auto'>
-            <div className='mx-auto w-[1200px] text-center text-3xl mt-20 flex items-center justify-center gap-4'>
+        <div className='max-container [w-1200px] m-auto flex  flex-col items-center justify-center h-full '>
+            <img src={buildingIMG} alt="build img" className="w-[300px] m-auto" />
+            <div className='text-center text-3xl flex items-center justify-center gap-4'>
                 <span>Bu sahifa tez orada qo'shiladi</span>
                 <IoIosGitNetwork />
             </div>
+            <Link to="/"><button className="btn btn-accent mt-10">Asosiy sahifaga qaytish</button></Link>
         </div>
     )
 }
